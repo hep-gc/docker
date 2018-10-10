09d2b8a9b3a409f4cb095b025b7a60b6513b4553
Danika MacDonell <danikam1@uvic.ca>
2018-10-10 16:36:48 -0700

    Added a hostname to run_condor.sh

--- external_cloud/separated_containers/single_host/condor/run_condor.sh
+++ external_cloud/separated_containers/single_host/condor/run_condor.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 docker build -t condor_container .
-docker run -itd --privileged --name condor_container -p 9618:9618 -p 40000-40500:40000-40500 condor_container
-
-while true; do sleep 1; done 
+docker run -itd --privileged --name condor_container --hostname condor_host -p 9618:9618 -p 40000-40500:40000-40500 condor_container
