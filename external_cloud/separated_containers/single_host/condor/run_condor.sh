cb4626eafe8270e952462d14bfaf72d2145682e9
Danika MacDonell <danikam1@uvic.ca>
2018-10-10 18:26:10 -0700

    Updated hostname of condor container to condor_host_container

--- external_cloud/separated_containers/single_host/condor/run_condor.sh
+++ external_cloud/separated_containers/single_host/condor/run_condor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build -t condor_container .
-docker run -itd --privileged --name condor_container --hostname condor_host -p 9618:9618 -p 40000-40500:40000-40500 condor_container
+docker run -itd --privileged --name condor_container --hostname condor_host_container -p 9618:9618 -p 40000-40500:40000-40500 condor_container
