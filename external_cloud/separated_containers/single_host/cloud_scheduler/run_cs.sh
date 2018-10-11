52be5a8dc1975f95fc799f646330f006cc603fdf
Danika MacDonell <danikam1@uvic.ca>
2018-10-10 20:38:13 -0700

    Updated the name of the condor host container to htcondor_host, updated the docker run scripts to specify the ip addresses of the docker containers, and updated condor_config.local in cloud_scheduler to set the condor host name to the specified ip address of the container running condor

--- external_cloud/separated_containers/single_host/cloud_scheduler/run_cs.sh
+++ external_cloud/separated_containers/single_host/cloud_scheduler/run_cs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build -t cs_container .
-docker run -itd --privileged --name cs_container --hostname cs_host cs_container
+docker run -itd --privileged --name cs_container --hostname cs_host --ip 172.17.0.3 cs_container
--- external_cloud/separated_containers/single_host/condor/run_condor.sh
+++ external_cloud/separated_containers/single_host/condor/run_condor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build -t condor_container .
-docker run -itd --privileged --name condor_container --hostname condor_host_container -p 9618:9618 -p 40000-40500:40000-40500 condor_container
+docker run -itd --privileged --name condor_container --hostname htcondor_host --ip 172.17.0.2 -p 9618:9618 -p 40000-40500:40000-40500 condor_container
