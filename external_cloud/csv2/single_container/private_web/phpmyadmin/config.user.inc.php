f1297003fe9a81b7becab7920f0e64c671d0016b
root <root@htc-dev.heprc.uvic.ca>
2018-11-12 10:47:39 -0800

    Removed obsolete files

--- external_cloud/csv2/single_container/private_web/phpmyadmin/config.user.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cfg['ForceSSL'] = true;
-
-$i = 0;
-$i++;
-$cfg['Servers'][$i]['ssl'] = true;
-$cfg['Servers'][$i]['ssl_key'] = '/data/ssl/server.key';
-$cfg['Servers'][$i]['ssl_cert'] = '/data/ssl/server.crt';
-?>
