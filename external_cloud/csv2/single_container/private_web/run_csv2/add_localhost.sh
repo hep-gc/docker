eb43b232a23e5d90a39a620603576d0277db9979
root <root@htc-dev.heprc.uvic.ca>
2018-11-21 22:41:39 -0800

    Added injection of correct condor IP address for NETWORK_INTERFACE in condor_config.local to initial setup script for run_csv2

--- external_cloud/csv2/single_container/private_web/run_csv2/add_localhost.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-LINE='172.28.5.1      localhost'; FILE=/etc/hosts; grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
---
+++ external_cloud/csv2/single_container/private_web/run_csv2/init_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Add localhost to the /etc/hosts file
+LINE='172.28.5.1      localhost'; FILE=/etc/hosts; grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+# Replace NETWORK_INTERFACE line in condor_config.local with the current host's IP address
+sed -i '/^NETWORK_INTERFACE =/ d' /etc/condor/condor_config.local
+LINE="NETWORK_INTERFACE = $public_ip"; FILE=/etc/condor/condor_config.local; grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
